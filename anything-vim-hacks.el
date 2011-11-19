ebb6c20c0f1073c06196154120aecb201f691726
Wataru MIYAGUNI <gonngo@gmail.com>
2011-11-19 20:35:52 +0900

    add vim-hacks:cache #3

--- anything-vim-hacks.el
+++ anything-vim-hacks.el
@@ -1,25 +1,46 @@
-(defconst vim-hacks:buffer-name "*vim-hacks*")
-(defconst vim-hacks:url "http://vim-users.jp/vim-hacks-project/")
+;;------------------------------
+;; Define constant variable
+;;------------------------------
+(defconst vim-hacks:buffer-name "*vim-hacks*"
+  "Buffer name used to `vim-hacks:http-get'")
 
-(put 'vim-hacks:exception-not-retrieved 'error-message "Vim Hacks - Not retrieved")
+(defconst vim-hacks:url "http://vim-users.jp/vim-hacks-project/"
+  "URL of Vim Hacks Project")
+
+(defconst vim-hacks:list-reverse nil
+  "If not nil and not t, display list in reverse order.")
+
+(defvar vim-hacks:cache '()
+  "Cache which stores vim hacks list obtained by `vim-hacks:get-hacks'")
+
+;;------------------------------
+;; Define exception error
+;;------------------------------
+(put 'vim-hacks:exception-not-retrieved 'error-message
+     "Vim Hacks - Not retrieved")
 (put 'vim-hacks:exception-not-retrieved 'error-conditions
      '(vim-hacks:exception-not-retrieved error))
 
-(defun vim-hacks:http-get ()
-  (ignore-errors (kill-buffer vim-hacks:buffer-name))
-  (unless (eq 0 (call-process "curl" nil `(,vim-hacks:buffer-name nil) nil
+;;------------------------------
+;; System Function
+;;------------------------------
+(defun vim-hacks:http-get (&optional buffer)
+  (if (null buffer) (setq buffer vim-hacks:buffer-name))
+  (ignore-errors (kill-buffer buffer))
+  (unless (eq 0 (call-process "curl" nil `(,buffer nil) nil
                               "-f"
                               "-X" "GET"
                               vim-hacks:url))
-    (signal 'vim-hacks:exception-not-retrieved "The requested URL returned error")))
+    (signal 'vim-hacks:exception-not-retrieved
+            "The requested URL returned error")))
 
-(defun vim-hacks:get-hacks (&optional buffer)
+(defun vim-hacks:parse-http-get-response (&optional buffer)
   (cond ((stringp buffer) (setq buffer (get-buffer buffer)))
         ((null buffer) (setq buffer vim-hacks:buffer-name)))
   (save-current-buffer
     (set-buffer buffer)
     (goto-char (point-min))
-    (let (bgin end lines)
+    (let (begin end lines)
       (re-search-forward "<h2>Hacks[^<]+</h2>[.\t\n]+\\(<ul>\\)" nil t)
       (setq begin (match-beginning 1))
       (setq end (search-forward "</ul>"))
@@ -31,9 +52,20 @@
                  (cdr (assq 'href (cadr atag))))))
        (xml-get-children (car (xml-parse-region begin end)) 'li)))))
 
+(defun vim-hacks:get-hacks-from-http ()
+  (vim-hacks:http-get)
+  (vim-hacks:parse-http-get-response))
+
+(defun vim-hacks:get-hacks ()
+  (if (= (safe-length vim-hacks:cache) 0)
+      (setq vim-hacks:cache (vim-hacks:get-hacks-from-http)))
+  vim-hacks:cache)
+
+;;------------------------------
+;; User Function
+;;------------------------------
 (defun vim-hacks:anything ()
   (interactive)
-  (vim-hacks:http-get)
   (anything
    `((name . "Vim hacks")
      (candidates . vim-hacks:get-hacks)
