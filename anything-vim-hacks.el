c8308e484b8f9aa01c94cff87da5ca3662cbe97e
Wataru MIYAGUNI <gonngo@gmail.com>
2011-11-20 02:01:29 +0900

    Markdown 形式で hack のページを表示できる一歩手前(ul や 引用以外) #1

--- anything-vim-hacks.el
+++ anything-vim-hacks.el
@@ -1,3 +1,5 @@
+(eval-when-compile (require 'cl))
+
 ;;------------------------------
 ;; Define constant variable
 ;;------------------------------
@@ -22,15 +24,63 @@
      '(vim-hacks:exception-not-retrieved error))
 
 ;;------------------------------
+;; View
+;;------------------------------
+(defun vim-hacks:view-a (node)
+  (concat "[" (caddr node) "]" "(" (xml-get-attribute node 'href) ")"))
+
+(defun vim-hacks:view-h2 (node)
+  (concat "\n## " (caddr node) "\n"))
+
+(defun vim-hacks:view-img (node)
+  (concat "![" (xml-get-attribute node 'alt) "](" (xml-get-attribute node 'src) ")"))
+
+(defun vim-hacks:view-pre-code (node)
+  (with-temp-buffer
+    (erase-buffer)
+    (insert (caddr node))
+    (goto-char (point-min))
+    (while (re-search-forward "^" nil t)
+      (replace-match "    " nil nil))
+    (goto-char (point-max))
+    (buffer-string)))
+
+(defun vim-hacks:view-code (node)
+  (concat "`" (caddr node) "`"))
+
+(defun vim-hacks:view-tree-scan (root)
+  (mapconcat
+   (lambda (x)
+     (cond ((stringp x) x)
+           ((listp x)
+            (let ((node-name (xml-node-name x)))
+              (cond ((eq node-name 'p)
+                     (concat "\n" (vim-hacks:view-tree-scan (cddr x)) "\n\n"))
+                    ((eq node-name 'a)
+                     (vim-hacks:view-a x))
+                    ((eq node-name 'img)
+                     (vim-hacks:view-img x))
+                    ((eq node-name 'h2)
+                     (vim-hacks:view-h2 x))
+                    ((eq node-name 'pre)
+                     (let ((code (xml-get-children x 'code)))
+                       (if code
+                           (vim-hacks:view-pre-code (car code)))))
+                    ((eq node-name 'code)
+                     (vim-hacks:view-code x))
+                    )))))
+   root ""))
+
+;;------------------------------
 ;; System Function
 ;;------------------------------
-(defun vim-hacks:http-get (&optional buffer)
+(defun vim-hacks:http-get (url &optional buffer)
   (if (null buffer) (setq buffer vim-hacks:buffer-name))
   (ignore-errors (kill-buffer buffer))
   (unless (eq 0 (call-process "curl" nil `(,buffer nil) nil
                               "-f"
                               "-X" "GET"
-                              vim-hacks:url))
+                              url))
     (signal 'vim-hacks:exception-not-retrieved
             "The requested URL returned error")))
 
@@ -40,7 +90,7 @@
   (save-current-buffer
     (set-buffer buffer)
     (goto-char (point-min))
-    (let (begin end lines)
+    (let (begin end)
       (re-search-forward "<h2>Hacks[^<]+</h2>[.\t\n]+\\(<ul>\\)" nil t)
       (setq begin (match-beginning 1))
       (setq end (search-forward "</ul>"))
@@ -52,15 +102,37 @@
                  (cdr (assq 'href (cadr atag))))))
        (xml-get-children (car (xml-parse-region begin end)) 'li)))))
 
+(defun vim-hacks:parse-view-http-get-response (&optional buffer)
+  (cond ((stringp buffer) (setq buffer (get-buffer buffer)))
+        ((null buffer) (setq buffer vim-hacks:buffer-name)))
+  (save-current-buffer
+    (set-buffer buffer)
+    (goto-char (point-min))
+    (let (begin end)
+      (search-forward "<div class=\"textBody\">" nil t)
+      (setq begin (point))
+      (insert "<div>")
+      (re-search-forward "<address class=\"hack-author\">.+</address>" nil t)
+      (insert "</div>")
+      (setq end (point))
+      (remove-if (lambda (x) (stringp x)) (cddar (xml-parse-region begin end))))))
+
 (defun vim-hacks:get-hacks-from-http ()
-  (vim-hacks:http-get)
+  (vim-hacks:http-get vim-hacks:url)
   (vim-hacks:parse-http-get-response))
+
+(defun vim-hacks:get-view-from-http (url)
+  (vim-hacks:http-get url)
+  (vim-hacks:parse-view-http-get-response))
 
 (defun vim-hacks:get-hacks ()
   (if (= (safe-length vim-hacks:cache) 0)
       (setq vim-hacks:cache (vim-hacks:get-hacks-from-http)))
   (if vim-hacks:list-reverse? (reverse vim-hacks:cache)
     vim-hacks:cache))
+
+(defun vim-hacks:view (url)
+  (insert (vim-hacks:view-tree-scan (vim-hacks:get-view-from-http url))))
 
 ;;------------------------------
 ;; User Function
@@ -71,7 +143,9 @@
    `((name . "Vim hacks")
      (candidates . vim-hacks:get-hacks)
      (action
-      ("Open Browser" . (lambda (x) (browse-url x))))
+      ("Open Browser" . (lambda (x) (browse-url x)))
+      ("View Hack"    . (lambda (x) (vim-hacks:view x)))
+      )
      )))
 
 
@@ -79,4 +153,3 @@
   (interactive)
   (setq vim-hacks:cache '())
   (vim-hacks:anything))
-
